fb08eb8bb2a9dcaef3354884bde93757cbd5a240
triplesyntax <gllanto87@gmail.com>
2019-03-31 10:43:52 +0800

    Updated list event and create event page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,31 +6,49 @@ import logo from './logo.svg'
 import CreateEvent from "./views/CreateEvent";
 import ListEvent from "./views/ListEvent";
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 class App extends Component {
+  
   render() {
     return (
       <Router>
+         <AppBar position="static">
+        <Toolbar>
+          <IconButton className="ssss" color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className="grow">
+            OutGoing
+          </Typography>
+          <Button component={Link} to="/" color="inherit">Event List</Button>
+          <Button component={Link} to="/create" color="inherit">Create Event</Button>
+        </Toolbar>
+      </AppBar>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
-            <Link to="/" className="navbar-brand">OutGoing</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
           <Route path="/create" exact component={CreateEvent} />
-          <Route path="/" component={ListEvent} />
+          <Route path="/" exact component={ListEvent} />
         </div>
       </Router>
     );
--- frontend/src/views/ListEvent.js
+++ frontend/src/views/ListEvent.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
+import MUIDataTable from "mui-datatables";
 
-export default class CreateEvent extends Component {
+const columns = [
+ {
+  name: "name",
+  label: "Name",
+  options: {
+   filter: true,
+   sort: true,
+  }
+ },
+ {
+  name: "company",
+  label: "Company",
+  options: {
+   filter: true,
+   sort: false,
+  }
+ },
+ {
+  name: "city",
+  label: "City",
+  options: {
+   filter: true,
+   sort: false,
+  }
+ },
+ {
+  name: "state",
+  label: "State",
+  options: {
+   filter: true,
+   sort: false,
+  }
+ },
+];
+
+const data = [
+ { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+ { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+ { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+ { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
+];
+
+const options = {
+  filterType: 'checkbox',
+};
+
+
+export default class ListEvent extends Component {
     render() {
         return (
             <div>
-                <p>Event List Page</p>
+                <MUIDataTable
+                  title={"Event List"}
+                  data={data}
+                  columns={columns}
+                  options={options}
+                />
             </div>
         )
     }

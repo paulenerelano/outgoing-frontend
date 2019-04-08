a587f179d744be84d398763809edc20c527124da
System Administrator <root@Lizas-MacBook-Air.local>
2019-04-09 02:38:21 +0800

    [up] implemented view details backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 
 app.use(cors());

d67ff8eaceb5767b8ce9b5fc500eb11321e79b5c
triplesyntax <gllanto87@gmail.com>
2019-04-13 19:25:23 +0800

    Updated base url

--- src/api/eventapi.js
+++ src/api/eventapi.js
@@ -14,7 +14,7 @@ module.exports = function() {
       const config = require("../config/config.js");
       const URL = config.HTTP_TAG + config.IP_ADDRESS + config.PORT
       console.log("------------------------------------------------" + URL);
-      return axios.get(HTTP_TAG + DB_ADDR + ":" + DB_PORT + "/event/list")
+      return axios.get(URL + "/event/list")
         .then(response => {
           return response;
         })
